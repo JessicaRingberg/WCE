5b66d9047f07ee12e62341b446a3919e54696449
Jessica Ringberg <jessicaringberg@outlook.com>
2022-02-25 12:31:16 +0100

    ok

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,12 @@
-// document.addEventListener("DOMContentLoaded", async () => {
-//     await fetchCourses();
-//     displayList(courseList);
-// });
-
-
-// class Kurs {
-//     constructor(input) {
-//         this.id = input.id;
-//         this.name = input.name;
-//         this.description = input.description;
-//         this.length = input.length;
-//         this.imgSrc = input.imgSrc
-//     }
-// }
-
-//async function fetchCourses() {
-//     const json = await fetch("courses.json")
-//         .then(res => res.text());
-//     courseList = JSON.parse(json);
-// }
-
-
-
-
-
-
-let courseList = [];
+//Array to hold courses from json file
+let jsonList = [];
+//Array to hold created courses
+let newCourse = JSON.parse(localStorage.getItem("p")) || [];
+//array that holds in both jsonList and newArray
+let coursesArray = [];
+//Array to hold items in cart
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 showCart();
-
 
 
 
@@ -38,10 +16,14 @@ showCart();
         .then((data) => {
             for (let i = 0; i < data.length; i++) {
                 const item = data[i];
-                courseList.push(item);
+                jsonList.push(item);
                
             }
-            displayList(courseList);
+            //Concat jsonlist with newCourse to create array that holds in both created and original courses
+            coursesArray = jsonList.concat(newCourse)
+
+            displayList();
+            
         })
     .catch(console.error);
 
@@ -49,13 +31,14 @@ showCart();
    
 
 
-//Show elements from json in html
-function displayList(array) {
+//Displays courses from array
+//Each element added gets a button with the function addToCart
+function displayList() {
 
-    document.getElementById("createN").innerHTML = "";
+    document.getElementById("show-cards").innerHTML = "";
 
-    for (let i = 0; i < array.length; i++) {
-        const course = array[i];
+    for (let i = 0; i < coursesArray.length; i++) {
+        const course = coursesArray[i];
         let courseDisplay = `<div class=" card-group col">
     <div class="card" style="width:15rem;">
       <img src="${course.imgSrc}" class="card-img-top" alt="kursbild">
@@ -69,63 +52,67 @@ function displayList(array) {
       </div>
     </div>
   </div>`
-        document.getElementById("createN").innerHTML += courseDisplay;
+        document.getElementById("show-cards").innerHTML += courseDisplay;
     }
+    //Save array to localstorage
+    localStorage.setItem("p", JSON.stringify(newCourse));
 
-}  
-
+}
 
 
 
 //Add course to array and display on site
 function addCourse() {
-  
-    courseList.push({
+    //If added course id already exists, then return and alert user
+    if (coursesArray.some((course) => course.id == document.getElementById("input-id").value)) {
+        alert("En kurs med detta id existerar redan!")
+        return;
+    }
+    //Add course to array with created courses
+    newCourse.push({
         id: document.getElementById("input-id").value,
         name: document.getElementById("input-name").value,
         description: document.getElementById("input-info").value,
         length: document.getElementById("input-length").value,
-        imgSrc: "./images/f.png"
+        imgSrc: "./images/new.png"
     
     });
-    //Get error not focusable..   document.getElementById("myForm").reset();
-    
-    console.log(courseList);
-    displayList(courseList);
-    console.log(courseList);
+
+    displayList();
 
 }
 
 
-
 // Add selected item to cart array
 function addToCart(id) {
+    //Prevents same course to be added twice
       if (cart.some((added) => added.id == id)) {
           return;
     }
-    const added = courseList.find(x => x.id == id);
-   
+    const added = coursesArray.find(x => x.id == id);
+   //add course to cart array
     cart.push(added);
+
     showCart();
 }
 
 
 
-// display items from cart in html
+// display items from cart array in html
 function showCart() {
     //Empty the html each time to avoid duplicates in cart display
     document.getElementById("my-things").innerHTML = "";
-
+//Each item gets a function added that lets you remove the item
     for (let i = 0; i < cart.length; i++) {
         const element = cart[i];
         let seen = `<li class="list-group-item justify-content-between align-items-center">
         <h5 onclick="deleteFromCart(${element.id})">
         ${element.name}<i class="bi bi-trash3 ms-md-auto">
-        </i></h5><small>${element.description}</small>` //Align trashcans later
+        </i></h5><small>${element.description}</small>`
         document.getElementById("my-things").innerHTML += seen;
         
     }
-    //Save cart
+    //Save cart to local storage
     localStorage.setItem("CART", JSON.stringify(cart));
 
 }
@@ -134,6 +121,7 @@ function showCart() {
 
 //Delete selected item in cart
 function deleteFromCart(id) {
+    //The clicked item gets removed from cart array
     cart = cart.filter((item) => item.id != id)
 
     showCart();
@@ -141,7 +129,7 @@ function deleteFromCart(id) {
 
 
 
-//Display modal to thank for purchase or alert cart is empty
+//Display modal to thank for purchase and empty cart array, or alert cart is empty
 function buyItems() {
     if (cart.length <= 0) {
         document.getElementById("confirmed").innerHTML = "Din kundvagn innehåller inga varor!";
