c72a9cd1ad22f0db3b8f927ab17e0d251ad17c5b
Jessica Ringberg <jessicaringberg@outlook.com>
2022-02-26 14:36:01 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 //Array to hold courses from json file
 let jsonList = [];
 //Array to hold created courses
-let newCourse = JSON.parse(localStorage.getItem("p")) || [];
-//array that holds in both jsonList and newArray
+let newCourse = JSON.parse(localStorage.getItem("x")) || [];
+//array that holds all courses
 let coursesArray = [];
 //Array to hold items in cart
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
@@ -19,7 +19,7 @@ showCart();
                 jsonList.push(item);
                
             }
-            //Concat jsonlist with newCourse to create array that holds in both created and original courses
+            //Concat jsonlist with newCourse to create array that holds both created and original courses
             coursesArray = jsonList.concat(newCourse)
 
             displayList();
@@ -45,7 +45,8 @@ function displayList() {
       <div class="card-body">
         <h5 class="card-title">${course.name}</h5>
         <small class="card-text">${course.description}</small>
-        <button class="btn btn-success
+        <p>${course.price} SEK</p>
+        <button id="courses-site" class="btn
         btn-sm add-to-cart" onclick="addToCart(${course.id})">
         <span><i class="bi bi-bag"></i></span>
         Lägg i kundvagn</button>
@@ -54,8 +55,8 @@ function displayList() {
   </div>`
         document.getElementById("show-cards").innerHTML += courseDisplay;
     }
-    //Save array to localstorage
-    localStorage.setItem("p", JSON.stringify(newCourse));
+    //Save to localstorage
+    localStorage.setItem("x", JSON.stringify(newCourse));
 
 }
 
@@ -63,17 +64,40 @@ function displayList() {
 
 //Add course to array and display on site
 function addCourse() {
+    let x = document.forms["myForm"]["input-id"].value;
+    if (x == "" || x == null) {
+        return false;
+    }
+    x = document.forms["myForm"]["input-name"].value;
+    if (x == "" || x == null) {
+        return false;
+    }
+    x = document.forms["myForm"]["input-length"].value;
+    if (x == "" || x == null) {
+        return false;
+    }
+    x = document.forms["myForm"]["input-info"].value;
+    if (x == "" || x == null) {
+        return false;
+    }
+     x = document.forms["myForm"]["input-price"].value;
+    if (x == "" || x == null) {
+        return false;
+    }
     //If added course id already exists, then return and alert user
     if (coursesArray.some((course) => course.id == document.getElementById("input-id").value)) {
         alert("En kurs med detta id existerar redan!")
-        return;
+        return false;
+        
     }
+
     //Add course to array with created courses
     newCourse.push({
         id: document.getElementById("input-id").value,
         name: document.getElementById("input-name").value,
         description: document.getElementById("input-info").value,
-        length: document.getElementById("input-length").value,
+        length: document.getElementById("input-length").value + "veckor",
+        price: document.getElementById("input-price").value,
         imgSrc: "./images/new.png"
     
     });
@@ -83,53 +107,70 @@ function addCourse() {
 }
 
 
-// Add selected item to cart array
+//Add selected item to cart
 function addToCart(id) {
     //Prevents same course to be added twice
-      if (cart.some((added) => added.id == id)) {
-          return;
+    if (cart.some((added) => added.id == id)) {
+        return;
     }
-    const added = coursesArray.find(x => x.id == id);
-   //add course to cart array
-    cart.push(added);
 
+    const added = coursesArray.find(x => x.id == id);
+    cart.push(added);
     showCart();
 }
 
 
+//Opens cart container
+function openCart() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
 
-// display items from cart array in html
+
+//Closes cart container
+function closeCart() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+//Display items in cart
 function showCart() {
+    //Count items in cart
+    let cartCount = 0;
+
     //Empty the html each time to avoid duplicates in cart display
     document.getElementById("my-things").innerHTML = "";
-//Each item gets a function added that lets you remove the item
+    let p = 0;
+   //Each item gets the deleteFromCart function
     for (let i = 0; i < cart.length; i++) {
         const element = cart[i];
+        p = p + parseInt(element.price);
         let seen = `<li class="list-group-item justify-content-between align-items-center">
         <h5 onclick="deleteFromCart(${element.id})">
         ${element.name}<i class="bi bi-trash3 ms-md-auto">
-        </i></h5><small>${element.description}</small>`
+        </i></h5>`
         document.getElementById("my-things").innerHTML += seen;
-        
+        cartCount++;
+
+
+
     }
+    document.getElementById("total-p").innerText = p + " SEK";
+    document.getElementById("cart-counter").innerText = cartCount;
+
     //Save cart to local storage
     localStorage.setItem("CART", JSON.stringify(cart));
 
 }
 
 
-
 //Delete selected item in cart
 function deleteFromCart(id) {
-    //The clicked item gets removed from cart array
     cart = cart.filter((item) => item.id != id)
-
     showCart();
 }
 
 
-
-//Display modal to thank for purchase and empty cart array, or alert cart is empty
+//Modal to confirm purchase
 function buyItems() {
     if (cart.length <= 0) {
         document.getElementById("confirmed").innerHTML = "Din kundvagn innehåller inga varor!";
@@ -139,5 +180,5 @@ function buyItems() {
         cart.length = 0;
         showCart();
     }
+    closeCart();
 }
-
